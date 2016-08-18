b46ac4fa50777687311758c743cd41f959999b0b
Rick Osowski <rosowski@gmail.com>
2016-08-17 23:34:53 -0500

    Initial commit for dinner stub service
    Signed-off-by: Rick Osowski <rosowski@gmail.com>

---
+++ src/main/java/com/ibm/microservices/wfd/EntreeApplication.java
@@ -0,0 +1,15 @@
+package com.ibm.microservices.wfd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class EntreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntreeApplication.class, args);
+		System.out.println("Running "+EntreeApplication.class+" via Spring Boot!");
+	}
+}
